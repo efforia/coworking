b1e1e8001b245f7d392be451d40b7974a7820c91
William Oliveira de Lagos <william.lagos@icloud.com>
2018-09-11 13:34:45 -0300

    Added initial source tree

---
+++ header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="{% block meta_keywords %}{% endblock %}">
+<meta name="description" content="{% block meta_description %}{% endblock %}">
+<title>Fábrica de Ideias Coworking</title>
+<link rel="shortcut icon" href="./static/img/fabricalogo.png">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="./static/css/efforia.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body class="full">
+
+<div class="navbar navbar-inverse fixed-navbar hidden-xs" style="background:rgba(0,0,0,0.75)">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">{% trans "Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" style="padding:10px; font-size:1.5em" href=".">
+        <img src="./static/svg/fabricalogo.svg" style="display:inline; height:1.5em; align:middle;"/>
+      </a>
+    </div>
+    <div class="navbar-collapse collapse" style="border:none; box-shadow:none;">
+      <ul class="nav navbar-nav" style="float:none; margin: 0 auto; display:table; table-layout:fixed;">
+        <li><a href="fotos">    Fotos     </a></li>
+        <li><a href="valores">   Valores   </a></li>
+        <li><a href="coworkers">  Coworkers </a></li>
+      </ul>
+    </div>
+  </div>
+</div>
---
+++ jumbotron.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="{% block meta_keywords %}{% endblock %}">
+<meta name="description" content="{% block meta_description %}{% endblock %}">
+<title>Fábrica de Ideias Coworking</title>
+<link rel="shortcut icon" href="./static/img/fabricalogo.png">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="./static/css/efforia.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body class="full nebula">
+
+<div class="navbar fixed-navbar hidden-xs" style="background:rgba(0,0,0,0.75)">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">{% trans "Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand hidden" style="padding:10px; font-size:1.5em" href="/">
+        <img src="./static/svg/fabricalogo.svg" style="display:inline; height:1.5em; align:middle;"/>
+      </a>
+    </div>
+    <div class="navbar-collapse collapse" style="border:none; box-shadow:none;">
+      <ul class="nav navbar-nav" style="float:none; margin: 0 auto; display:table; table-layout:fixed;">
+        <li><a href="fotos">    Fotos     </a></li>
+        <li><a href="valores">   Valores   </a></li>
+        <li><a href="coworkers">  Coworkers </a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron">
+  <div class="container text-center">
+    <img src="./static/svg/fabrica.svg" class="hidden-xs img-responsive center-block" style="width:20%; margin:0 auto;"/>
+    <img src="./static/svg/fabrica.svg" class="visible-xs img-responsive center-block" style="width:30%; margin:0 auto;"/>
+    <h2 style="font-size:2em; line-height:1.2em; padding-top:1em;">
+      Somos a Fábrica de Ideias Coworking, <br>um ambiente coletivo completamente acolhedor.<br></h2>
+    <button class="btn btn-default btn-lg" onclick="window.location='http://www.youtube.com/watch/BEhubwgjl6s'"style="margin-top:2em; background:transparent; color:white; border: 1px solid white;">
+	<h2 class="hidden-xs" style="font-size:1.5em; margin-top:1px; margin-bottom:0px">Saiba mais o que é o coworking</h2>
+	<h2 class="visible-xs" style="font-size:1.5em; margin:0px">Saiba mais</h2>
+    </button>
+  </div>
+</div>
