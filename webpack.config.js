ef623a2ce327ec417a8196a8b632afc76de5edbe
Leonardo Quevedo <lpachecoquevedo@gmail.com>
2018-10-05 17:04:40 -0300

    refact(general): Adds UI style modifications to the landing page.

--- webpack.config.js
+++ webpack.config.js
@@ -53,34 +53,9 @@ const config = {
             // file-loader(for images)
             { test: /\.(jpg|png|gif)$/, use: [{ loader: "file-loader", options: { name: "[path][name].[ext]" } }] },
             // file-loader(for fonts)
-            { test: /\.(woff|woff2|eot|ttf|otf)$/, use: [{ loader: "file-loader", options: { name: "[path][name].[ext]" } }] },
+            { test: /\.(woff|woff2|eot|ttf|otf|svg)$/, use: [{ loader: "file-loader", options: { name: "[path][name].[ext]" } }] },
             // file-loader(for favicon)
             { test: /\.(ico)$/, use: [{ loader: "file-loader", options: { name: "[path][name].[ext]" } }] },
-            {
-                test: /\.(svg)$/,
-                exclude: /fonts/, /* dont want svg fonts from fonts folder to be included */
-                use: [
-                    {
-                        loader: 'svg-url-loader',
-                        options: {
-                            noquotes: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                exclude: /images/,  /* dont want svg images from image folder to be included */
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts/',
-                            name: '[name][hash].[ext]',
-                        },
-                    },
-                ],
-            }
         ]
     },
     plugins: [
